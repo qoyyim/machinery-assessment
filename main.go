ace74ed0ffa044909bc23bffddd4efa358c7909b
ibnul.qoyyim <ibnul.qoyyim@grabtaxi.com>
2020-04-13 15:06:25 +0700

    trying pending and retry

--- main.go
+++ main.go
@@ -30,34 +30,56 @@ func main() {
 		return
 		// do something with the error
 	}
-	server.RegisterTask("add", Add)
+	server.RegisterTask("success", AlwaysSuccess)
+	server.RegisterTask("fail", AlwaysFail)
 
-	worker := server.NewWorker("test_worker", 0)
+	worker := server.NewWorker("test_worker", 1)
 	go worker.Launch()
 	defer worker.Quit()
 
+	start := time.Now()
 	signature := &tasks.Signature{
-		Name: "add",
+		Name: "fail",
 		Args: []tasks.Arg{
 			{
 				Type:  "int64",
-				Value: 1,
+				Value: start.Unix(),
+			}, {
+				Type:  "int64",
+				Value: start.UnixNano(),
 			},
+		},
+	}
+	// If the task fails, retry it up to 3 times
+	signature.RetryCount = 4
+
+	// Delay the task by 5 seconds
+	//eta := time.Now().UTC().Add(time.Second * 5)
+	//signature.ETA = &eta
+
+	asyncResult, err := server.SendTask(signature)
+
+	signature = &tasks.Signature{
+		Name: "success",
+		Args: []tasks.Arg{
 			{
 				Type:  "int64",
-				Value: 1,
+				Value: start.Unix(),
+			}, {
+				Type:  "int64",
+				Value: start.UnixNano(),
 			},
 		},
 	}
 
-	asyncResult, err := server.SendTask(signature)
+	asyncResult, err = server.SendTask(signature)
 	if err != nil {
 		fmt.Printf("cant send task", err)
 		return
 		// failed to send the task
 		// do something with the error
 	}
-	results, err := asyncResult.Get(time.Duration(time.Millisecond * 5))
+	results, err := asyncResult.Get(time.Duration(time.Second * 15))
 	if err != nil {
 		// getting result of a task failed
 		// do something with the error
@@ -69,11 +91,20 @@ func main() {
 	fmt.Printf("all ok")
 }
 
-func Add(args ...int64) (int64, error) {
-	sum := int64(0)
-	for _, arg := range args {
-		sum += arg
-	}
-	fmt.Printf("return")
-	return sum, nil
+func AlwaysSuccess(sec int64, nano int64) error {
+	fmt.Print("success")
+	fmt.Printf("\nqueued at", time.Unix(sec, 0))
+	time.Sleep(1 * time.Second)
+	end := time.Now()
+	fmt.Printf("\nexecuted at", end)
+	return nil
+}
+
+func AlwaysFail(sec int64, nano int64) error {
+	fmt.Print("fail")
+	fmt.Printf("\nqueued at", time.Unix(sec, 0))
+	time.Sleep(1 * time.Second)
+	end := time.Now()
+	fmt.Printf("\nexecuted at", end)
+	return fmt.Errorf("testing retries")
 }
